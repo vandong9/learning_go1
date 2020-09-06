bc1c2d9d7b2e3a01869e50561d426e74937f2ed2
dong ha <van_dong9@yahoo.com>
2020-09-07 00:04:25 +0700

    add hello word app

---
+++ freetour/hello.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	fmt.Println("Hello .... world ???")
+	fmt.Println("The time is now: ", time.Now())
+	fmt.Println("Random value: ", rand.Intn(100))
+}
